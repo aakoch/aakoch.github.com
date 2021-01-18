da16cfbbdf95ed3b38d8a60721e296b42a715818
Adam Koch <adam@dogtired.org>
2021-01-17 21:09:05 -0600

    moved to new bootstrap and parcel

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+import 'bootstrap';
 //// First lets create our drawing surface out of existing SVG element
 //// If you want to create new surface just provide dimensions
 //// like s = Snap(800, 600);
