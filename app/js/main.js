b438969b06bb93de7fec09258e8961cf35b7ef5e
Adam Koch <adam@dogtired.org>
2021-01-18 19:21:34 -0600

    fix image (again)

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,6 @@
 import 'bootstrap';
 
-let image = new Image();
-image.src = require("../assets/images/koch_motel.jpg");
+import images from "../assets/images/*.jpg";
 //// First lets create our drawing surface out of existing SVG element
 //// If you want to create new surface just provide dimensions
 //// like s = Snap(800, 600);
