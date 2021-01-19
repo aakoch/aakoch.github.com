695b34f4e5347a57ad98dc979540c888be299d0f
Adam Koch <adam@dogtired.org>
2021-01-18 20:23:17 -0600

    fixed image (again); twitter icon smaller when built

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 import 'bootstrap';
 
-import images from "../assets/images/*.jpg";
 //// First lets create our drawing surface out of existing SVG element
 //// If you want to create new surface just provide dimensions
 //// like s = Snap(800, 600);
