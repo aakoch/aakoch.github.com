aa6faa76640910a351c64c9fbaa86b50e1ba6e4d
aakoch <aakoch@users.noreply.github.com>
2021-02-20 19:11:10 +0000

    deploy: e84463ec5ff3bf653272f957973772c5a786e15c

--- main.c81c80c1.js
+++ main.c81c80c1.js
@@ -119,4 +119,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"WdKE":[function(require,module,exports) {
 "use strict";require("bootstrap"),require("../sass/custom.scss");
 },{"bootstrap":"emzB","../sass/custom.scss":"hfIV"}]},{},["WdKE"], null)
-//# sourceMappingURL=main.11ebfe6e.js.map
+//# sourceMappingURL=main.c81c80c1.js.map
